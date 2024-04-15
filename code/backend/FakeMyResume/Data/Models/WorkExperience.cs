496d1a4d99dad261d710801f61f26efe88c161de
Jorge Fernandez <jorge.fernandez@unosquare.com>
2024-04-15 11:22:48 -0500

    Add data layer

---
+++ code/backend/FakeMyResume/Data/Models/WorkExperience.cs
@@ -0,0 +1,22 @@
+﻿namespace FakeMyResume.Data.Models;
+
+public class WorkExperience
+{
+    public int FromYear { get; set; }
+
+    public int FromMonth { get; set; }
+
+    public int ToYear { get; set; }
+
+    public int ToMonth { get; set; }
+
+    public string? CompanyName { get; set; }
+
+    public string? ProjectName { get; set; }
+
+    public string? Role { get; set; }
+
+    public string? Description { get; set; }
+
+    public List<Project>? Projects { get; set; }
+}
