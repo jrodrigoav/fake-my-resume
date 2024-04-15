818daa5d57c3d3227de5fe1846d39aea2bd0c0d4
Jorge Fernandez <jorge.fernandez@unosquare.com>
2024-04-15 11:22:47 -0500

    Add home page with app navigation

--- code/frontend/fakemyresume/src/app/app.component.ts
+++ code/frontend/fakemyresume/src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ CommonModule, RouterLink, RouterOutlet, MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fakemyresume';
+  pageLinks = [
+    { label: 'Home', route: '/home' },
+  ];
+
+  loggedIn: Observable<boolean> = of(true);
+
+  login() {
+
+  }
+
+  logout() {
+    
+  }
 }
