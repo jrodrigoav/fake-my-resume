790ebb6e9aa89e83a3f54271bf2a64897159072c
Jorge Fernandez <jorge.fernandez@unosquare.com>
2024-04-15 11:22:48 -0500

    Add date adapter for material date picker

--- code/frontend/fakemyresume/src/app/app.config.ts
+++ code/frontend/fakemyresume/src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { provideEnvironment } from './interfaces/environment-config';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideHttpClient(),
     provideEnvironment(),
+    provideNativeDateAdapter(),
   ]
 };
