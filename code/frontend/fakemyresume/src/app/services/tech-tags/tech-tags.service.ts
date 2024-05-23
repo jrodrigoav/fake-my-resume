65e263fbf29271ff79a0558eeeaf7a08d07a54fa
Emilio Galvan <egalvan@foundationmedicine.com>
2024-05-23 08:57:56 -0600

    changing Tags route

--- code/frontend/fakemyresume/src/app/services/tech-tags/tech-tags.service.ts
+++ code/frontend/fakemyresume/src/app/services/tech-tags/tech-tags.service.ts
@@ -15,7 +15,6 @@ export class TechTagsService {
 
   searchTechTags(tagName: string): Observable<string[]> {
     const searchParams = { text: tagName };
-    return this.http.get<string[]>(this.baseUrl+"/resume/techs", { params: searchParams });//TODO change this to tag
+    return this.http.get<string[]>(this.baseUrl+"/tag", { params: searchParams });//TODO change this to tag
   }
- 
 }
